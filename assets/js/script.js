80db5d2aad163af84dcae2bd48f6ffa5cf4e884d
Emeka Elo-Chukwuma <elochux@gmail.com>
2021-04-22 18:10:13 +0100

    Fixed bugs in manifest and service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,131 @@
+let signature = document.querySelector(".love");
 
+let region = document.createElement("h2");
+region.className = "location";
+signature.insertAdjacentElement("beforebegin", region);
 
+let receivingTime = document.createElement("p");
+receivingTime.className = "date";
+signature.insertAdjacentElement("beforebegin", receivingTime);
 
-let signature = document.querySelector('.love');
+let weatherWrapper = document.createElement("div");
+weatherWrapper.className = "weather";
+weatherWrapper.innerHTML =
+  '<div class="weather__details"><img class="weather__icon" src="">            <p class="weather__description"></p>        </div><div class="current-temperature">            <p></p>        </div>    </div> ';
+signature.insertAdjacentElement("beforebegin", weatherWrapper);
 
-let region = document.createElement('h2');
-region.className = 'location';
-signature.insertAdjacentElement('beforebegin', region);
+let sunTimes = document.createElement("div");
+sunTimes.className = "sun-times";
+sunTimes.innerHTML =
+  '<div class="sunrise">            <p class="text-bold">Sunrise</p>            <p class="sunrise__time"></p>        </div>        <div class="sunset">            <p class="text-bold">Sunset</p>            <p class="sunset__time"></p>        </div>';
+signature.insertAdjacentElement("beforebegin", sunTimes);
 
-let receivingTime = document.createElement('p');
-receivingTime.className = 'date';
-signature.insertAdjacentElement('beforebegin', receivingTime);
+let weatherElements = document.createElement("div");
+weatherElements.className = "weather__elements";
+weatherElements.innerHTML =
+  '<div class="cloudiness">            <p>Cloudiness</p>            <p class="cloudiness__value"></p>            <p>%</p>        </div>        <div class="humidity">            <p>Humidity</p>            <p class="humidity__value"></p>            <p>%</p>        </div>        <div class="pressure">            <p>Pressure</p>            <p class="pressure__value"></p>            <p>kPa</p>        </div>        <div class="wind">            <p>Wind</p>            <p class="wind__value"></p>            <p>km/h</p>        </div>';
+signature.insertAdjacentElement("beforebegin", weatherElements);
 
-let weatherWrapper = document.createElement('div');
-weatherWrapper.className = 'weather';
-weatherWrapper.innerHTML = '<div class="weather__details"><img class="weather__icon" src="">            <p class="weather__description"></p>        </div><div class="current-temperature">            <p></p>        </div>    </div> '
-signature.insertAdjacentElement('beforebegin', weatherWrapper);
-
-let sunTimes = document.createElement('div');
-sunTimes.className = 'sun-times';
-sunTimes.innerHTML = '<div class="sunrise">            <p class="text-bold">Sunrise</p>            <p class="sunrise__time"></p>        </div>        <div class="sunset">            <p class="text-bold">Sunset</p>            <p class="sunset__time"></p>        </div>';
-signature.insertAdjacentElement('beforebegin', sunTimes);
-
-let weatherElements = document.createElement('div');
-weatherElements.className = 'weather__elements';
-weatherElements.innerHTML = '<div class="cloudiness">            <p>Cloudiness</p>            <p class="cloudiness__value"></p>            <p>%</p>        </div>        <div class="humidity">            <p>Humidity</p>            <p class="humidity__value"></p>            <p>%</p>        </div>        <div class="pressure">            <p>Pressure</p>            <p class="pressure__value"></p>            <p>kPa</p>        </div>        <div class="wind">            <p>Wind</p>            <p class="wind__value"></p>            <p>km/h</p>        </div>';
-signature.insertAdjacentElement('beforebegin', weatherElements);
-
-
-
-let searchField = document.querySelector('.search');
-let searchBtn = document.querySelector('.search-icon');
+let searchField = document.querySelector(".search");
+let searchBtn = document.querySelector(".search-icon");
 //let region = document.querySelector('.location');
 //let receivingTime = document.querySelector('.date');
-let weatherIcon = document.querySelector('.weather__icon');
-let weatherDescription = document.querySelector('.weather__description');
-let currentTemp = document.querySelector('.current-temperature p');
-let sunrise = document.querySelector('.sunrise__time');
-let sunset = document.querySelector('.sunset__time');
-let cloudValue = document.querySelector('.cloudiness__value');
-let humidityValue = document.querySelector('.humidity__value');
-let pressureValue = document.querySelector('.pressure__value');
-let windValue = document.querySelector('.wind__value');
+let weatherIcon = document.querySelector(".weather__icon");
+let weatherDescription = document.querySelector(".weather__description");
+let currentTemp = document.querySelector(".current-temperature p");
+let sunrise = document.querySelector(".sunrise__time");
+let sunset = document.querySelector(".sunset__time");
+let cloudValue = document.querySelector(".cloudiness__value");
+let humidityValue = document.querySelector(".humidity__value");
+let pressureValue = document.querySelector(".pressure__value");
+let windValue = document.querySelector(".wind__value");
 
 let jsonWeatherData;
-let weatherData = JSON.parse(localStorage.getItem('weatherData'));
+let weatherData = JSON.parse(localStorage.getItem("weatherData"));
 
 if (weatherData != null) {
-    region.innerHTML = weatherData.name;
-    receivingTime.innerHTML = new Date(weatherData.dt * 1000).toLocaleString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
+  region.innerHTML = weatherData.name;
+  receivingTime.innerHTML = new Date(weatherData.dt * 1000).toLocaleString(
+    "en-US",
+    {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    }
+  );
 
-    // weather icon
-    let iconcode = weatherData.weather[0].icon;
-    let iconurl = `https://openweathermap.org/img/wn/${iconcode}@2x.png`;
-    weatherIcon.setAttribute('src', iconurl);
+  // weather icon
+  let iconcode = weatherData.weather[0].icon;
+  let iconurl = `https://openweathermap.org/img/wn/${iconcode}@2x.png`;
+  weatherIcon.setAttribute("src", iconurl);
 
-    weatherDescription.innerHTML = weatherData.weather[0].description;
-    currentTemp.innerHTML = (weatherData.main.temp - 273.15).toPrecision(2) + '°C';
+  weatherDescription.innerHTML = weatherData.weather[0].description;
+  currentTemp.innerHTML =
+    (weatherData.main.temp - 273.15).toPrecision(2) + "°C";
 
-    sunrise.innerHTML = new Date(weatherData.sys.sunrise * 1000).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' });
-    sunset.innerHTML = new Date(weatherData.sys.sunset * 1000).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' });
-    cloudValue.innerHTML = weatherData.clouds.all;
-    humidityValue.innerHTML = weatherData.main.humidity;
-    pressureValue.innerHTML = weatherData.main.pressure / 10;
-    windValue.innerHTML = weatherData.wind.speed;
+  sunrise.innerHTML = new Date(
+    weatherData.sys.sunrise * 1000
+  ).toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+  sunset.innerHTML = new Date(
+    weatherData.sys.sunset * 1000
+  ).toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+  cloudValue.innerHTML = weatherData.clouds.all;
+  humidityValue.innerHTML = weatherData.main.humidity;
+  pressureValue.innerHTML = weatherData.main.pressure / 10;
+  windValue.innerHTML = weatherData.wind.speed;
 }
-
 
 // Function for API request
 let weatherReq = () => {
-    searchValue = document.querySelector('.search').value;
-    console.log(searchValue);
-    // Api request
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=cb5bf741dc48f795025eed310da9f370`).then((resp) => {
-        return resp.json(); // converts data to json
-    }).then((data) => {
-        /* weatherArr.push(data);
+  searchValue = document.querySelector(".search").value;
+  //console.log(searchValue);
+  // Api request
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=${process.env.API_KEY}`
+  )
+    .then((resp) => {
+      return resp.json(); // converts json to object
+    })
+    .then((data) => {
+      /* weatherArr.push(data);
         console.log(weatherArr); */
-        localStorage.setItem('weatherData', JSON.stringify(data));
-        jsonWeatherData = localStorage.getItem('weatherData');
-        region.innerHTML = data.name; // location
-        receivingTime.innerHTML = new Date(data.dt * 1000).toLocaleString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
+      localStorage.setItem("weatherData", JSON.stringify(data));
+      jsonWeatherData = localStorage.getItem("weatherData");
+      region.innerHTML = data.name; // location
+      receivingTime.innerHTML = new Date(data.dt * 1000).toLocaleString(
+        "en-US",
+        { weekday: "long", month: "long", day: "numeric", year: "numeric" }
+      );
 
-        // weather icon
-        let iconcode = data.weather[0].icon;
-        let iconurl = `https://openweathermap.org/img/wn/${iconcode}@2x.png`;
-        weatherIcon.setAttribute('src', iconurl);
+      // weather icon
+      let iconcode = data.weather[0].icon;
+      let iconurl = `https://openweathermap.org/img/wn/${iconcode}@2x.png`;
+      weatherIcon.setAttribute("src", iconurl);
 
-        weatherDescription.innerHTML = data.weather[0].description;
-        currentTemp.innerHTML = (data.main.temp - 273.15).toPrecision(2) + '°C';
+      weatherDescription.innerHTML = data.weather[0].description;
+      currentTemp.innerHTML = (data.main.temp - 273.15).toPrecision(2) + "°C";
 
-        sunrise.innerHTML = new Date(data.sys.sunrise * 1000).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' });
-        sunset.innerHTML = new Date(data.sys.sunset * 1000).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' });
-        cloudValue.innerHTML = data.clouds.all;
-        humidityValue.innerHTML = data.main.humidity;
-        pressureValue.innerHTML = data.main.pressure / 10;
-        windValue.innerHTML = data.wind.speed;
+      sunrise.innerHTML = new Date(
+        data.sys.sunrise * 1000
+      ).toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+      sunset.innerHTML = new Date(
+        data.sys.sunset * 1000
+      ).toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
+      cloudValue.innerHTML = data.clouds.all;
+      humidityValue.innerHTML = data.main.humidity;
+      pressureValue.innerHTML = data.main.pressure / 10;
+      windValue.innerHTML = data.wind.speed;
+    })
+    .catch(() => {});
+};
 
-    }).catch(() => {
-
-    });
-}
-
-
-searchBtn.addEventListener('click', weatherReq);
-searchField.addEventListener('keyup', (event) => { // listen for the enter keypress
-    if (event.keyCode === 13) {
-        weatherReq();
-    }
+searchBtn.addEventListener("click", weatherReq);
+searchField.addEventListener("keyup", (event) => {
+  // listen for the enter keypress
+  if (event.keyCode === 13) {
+    weatherReq();
+  }
 });
 
 /* function initweather() {
@@ -115,8 +134,9 @@ searchValue = 'London'
 } */
 
 // Check and register service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then((reg) => console.log('service worker registered',reg))
-        .catch((err) => console.log('service worker not registered',err))
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then((reg) => {} /* console.log("service worker registered", reg) */)
+    .catch((err) => {} /* console.log("service worker not registered", err) */);
 }
--- sw.js
+++ sw.js
@@ -1,71 +1,80 @@
-
 const weatherlyApp = "WeatherLy-4.0";
-const dynamicWeatherlyApp = "dynamic-WeatherLy-1.0" // for dynamic caching of resources not pre cached
+const dynamicWeatherlyApp = "dynamic-WeatherLy-1.0"; // for dynamic caching of resources not pre cached
 const assets = [
-    "/",
-    "index.html",
-    "assets/css/style.css",
-    "assets/js/script.js",
-    "assets/img/ic.png",
-    "assets/img/ic512.png",
-    "https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;500;600&display=swap",
-    "https://fonts.gstatic.com/s/chakrapetch/v4/cIf6MapbsEk7TDLdtEz1BwkWkKpgar3I1D8t.woff2",
-    "https://fonts.gstatic.com/s/chakrapetch/v4/cIf6MapbsEk7TDLdtEz1BwkWn6pgar3I1A.woff2",
-    "https://fonts.gstatic.com/s/chakrapetch/v4/cIflMapbsEk7TDLdtEz1BwkeQI51R5_F_gUk0w.woff2",
-    "https://fonts.gstatic.com/s/chakrapetch/v4/cIflMapbsEk7TDLdtEz1BwkebIl1R5_F_gUk0w.woff2",
-    "manifest.json"
-]
-
+  "/",
+  "index.html",
+  "assets/css/style.css",
+  "assets/js/script.js",
+  "assets/img/ic.png",
+  "assets/img/ic512.png",
+  "https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;500;600&display=swap",
+  "https://fonts.gstatic.com/s/chakrapetch/v4/cIf6MapbsEk7TDLdtEz1BwkWkKpgar3I1D8t.woff2",
+  "https://fonts.gstatic.com/s/chakrapetch/v4/cIf6MapbsEk7TDLdtEz1BwkWn6pgar3I1A.woff2",
+  "https://fonts.gstatic.com/s/chakrapetch/v4/cIflMapbsEk7TDLdtEz1BwkeQI51R5_F_gUk0w.woff2",
+  "https://fonts.gstatic.com/s/chakrapetch/v4/cIflMapbsEk7TDLdtEz1BwkebIl1R5_F_gUk0w.woff2",
+  "manifest.json",
+];
 
 // cache size limit function
 const limitCacheSize = (name, size) => {
-    caches.open(name).then(cache => {
-        cache.keys().then(keys => {
-            if (keys.length > size) {
-                cache.delete(keys[0]).then(limitCacheSize(name, size)); // keeps calling itself until the if condition is no longer true
-            }
-        })
-    })
-}
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size)); // keeps calling itself until the if condition is no longer true
+      }
+    });
+  });
+};
 
 // install service worker
-self.addEventListener('install', installEvent => {
-    //console.log('service worker has been installed');
-    installEvent.waitUntil( //lets the installEvent function wait until the caching is done
-        caches.open(weatherlyApp).then(cache => {
-            console.log('caching shell assets');
-            cache.addAll(assets); // pre-caching
-        })
-    );
+self.addEventListener("install", (installEvent) => {
+  // Self is the service worker itself which enables us to listen to lifecycle events
+  //console.log('service worker has been installed');
+  installEvent.waitUntil(
+    //lets the installEvent function wait until the caching is done
+    caches.open(weatherlyApp).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets); // pre-caching
+    })
+  );
 });
 
 // activate event
-self.addEventListener('activate', activateEvent => {
-    //console.log('service worker has been activated');
-    activateEvent.waitUntil(
-        caches.keys().then(keys => {
-            console.log(keys);
-            return Promise.all(keys.filter(key => key !== weatherlyApp && key !== dynamicWeatherlyApp).map(key => caches.delete(key)) //deletes old caches
-            )
-        })
-    )
+self.addEventListener("activate", (activateEvent) => {
+  //console.log('service worker has been activated');
+  activateEvent.waitUntil(
+    caches.keys().then((keys) => {
+      console.log(keys);
+      return Promise.all(
+        keys
+          .filter((key) => key !== weatherlyApp && key !== dynamicWeatherlyApp)
+          .map((key) => caches.delete(key)) //deletes old caches
+      );
+    })
+  );
 });
 
 // fetch event
-self.addEventListener('fetch', fetchEvent => {
-    //console.log('fetch event', fetchEvent);
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(cachesRes => {
-            return cachesRes || fetch(fetchEvent.request).then(fetchRes => { //returns cached resource if available else carries on with the fetch
-                return caches.open(dynamicWeatherlyApp).then(cache => { // for inserting in dynamic cache
-                    cache.put(fetchEvent.request.url, fetchRes.clone());
-                    limitCacheSize(dynamicWeatherlyApp, 5); //limits the number of items in the cache
-                    return fetchRes;
-                })
-            });
+self.addEventListener("fetch", (fetchEvent) => {
+  //console.log('fetch event', fetchEvent);
+
+  // check if request is made by chrome extensions or web page
+  // if request is made for web page url must contains http.
+  if (!(fetchEvent.request.url.indexOf("http") === 0)) return; // skip the request. if request is not made with http protocol
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((cachesRes) => {
+      return (
+        cachesRes ||
+        fetch(fetchEvent.request).then((fetchRes) => {
+          //returns cached resource if available else carries on with the fetch
+          return caches.open(dynamicWeatherlyApp).then((cache) => {
+            // for inserting in dynamic cache
+            cache.put(fetchEvent.request.url, fetchRes.clone());
+            limitCacheSize(dynamicWeatherlyApp, 5); //limits the number of items in the cache
+            return fetchRes;
+          });
         })
-    )
+      );
+    })
+  );
 });
-
-
-
